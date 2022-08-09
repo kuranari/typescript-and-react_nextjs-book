13f789c12eb5bc0cffd6f390ab9dd120320612bb
Tomohisa Kuranari <tomohisa.kuranari@gmail.com>
2022-08-09 14:23:52 +0900

    Modify .storybook to change infomation by storybook

--- next-sample/stories/StyledButton.stories.tsx
+++ next-sample/stories/StyledButton.stories.tsx
@@ -1,42 +1,59 @@
 import React, { useState } from "react"
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { StyledButton } from "../components/StyledButton";
 import { action } from "@storybook/addon-actions"
 
 export default {
   title: "StyledButton",
   compoment: StyledButton,
-  argTypes: { onClick: { action: 'clicked '}}
+  argTypes: { 
+    onClick: { action: 'clicked '},
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'success', 'transparent'],
+    },
+    children: {
+      control: { type: 'text' },
+    },
+  }
 } as ComponentMeta<typeof StyledButton>;
 
-const incrementAction = action('increment')
+const Template: ComponentStory<typeof StyledButton> = (args) => <StyledButton {...args} />
 
-export const Primary = (props) => {
-  const [count, setCount] = useState(0)
-  const onClick = (e: React.MouseEvent) => {
-    incrementAction(e, count)
-    setCount((c) => c + 1)
-  }
+export const TemplateTest = Template.bind({})
 
-  return (
-    <StyledButton {...props} variant="primary" onClick={onClick}>
-      Count: {count}
-    </StyledButton>
-  );
-};
+TemplateTest.args = {
+  variant: 'primary',
+  children: 'Primary'
+}
+// const incrementAction = action('increment')
 
-export const Success = (props) => {
-  return (
-    <StyledButton {...props} variant="success">
-      Success
-    </StyledButton>
-  );
-};
+// export const Primary = (props) => {
+//   const [count, setCount] = useState(0)
+//   const onClick = (e: React.MouseEvent) => {
+//     incrementAction(e, count)
+//     setCount((c) => c + 1)
+//   }
 
-export const Transparent = (props) => {
-  return (
-    <StyledButton {...props} variant="transparent">
-      Transparent
-    </StyledButton>
-  );
-};
+//   return (
+//     <StyledButton {...props} variant="primary" onClick={onClick}>
+//       Count: {count}
+//     </StyledButton>
+//   );
+// };
+
+// export const Success = (props) => {
+//   return (
+//     <StyledButton {...props} variant="success">
+//       Success
+//     </StyledButton>
+//   );
+// };
+
+// export const Transparent = (props) => {
+//   return (
+//     <StyledButton {...props} variant="transparent">
+//       Transparent
+//     </StyledButton>
+//   );
+// };
