81b816ecf10490d00c029c3be7b70d6f2ce66ffd
Tomohisa Kuranari <tomohisa.kuranari@gmail.com>
2022-08-09 11:21:55 +0900

    Add posts/[id].tsx

---
+++ next-sample/pages/posts/[id].tsx
@@ -0,0 +1,70 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { ParsedUrlQuery } from 'node:querystring'
+
+type PostProps = {
+  id: string
+}
+
+const Post: NextPage<PostProps> = (props) => {
+  const { id } = props
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>このページは静的サイト生成によってビルド時に生成されたページです。</p>
+        <p>{`/posts/${id}に対応するページです`}</p>
+      </main>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // それぞれのページのパスパラメータをまとめたもの
+  const paths = [
+    {
+      params: {
+        id: '1',
+      },
+    },
+    {
+      params: {
+        id: '2',
+      },
+    },
+    {
+      params: {
+        id: '3',
+      },
+    },
+  ]
+
+  // fallbackをfalseにすると、pathsで定義されたページ以外は404ページを表示する
+  return { paths, fallback: false }
+}
+
+// パラメータの型を定義
+interface PostParams extends ParsedUrlQuery {
+  id: string
+}
+
+// getStaticPaths実行後にそれぞれのパスに対してgetStaticPropsが実行される
+export const getStaticProps: GetStaticProps<PostProps, PostParams> = async (context) => {
+  return {
+    props: {
+      id: context.params!['id'],
+    },
+  }
+}
+
+export default Post
