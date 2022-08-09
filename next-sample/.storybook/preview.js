5f9ae38b58aa5d5895de07eef30ed2ffa09c2a7a
Tomohisa Kuranari <tomohisa.kuranari@gmail.com>
2022-08-09 15:11:54 +0900

    Add preview settings

--- next-sample/.storybook/preview.js
+++ next-sample/.storybook/preview.js
@@ -6,4 +6,23 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      iphonex: {
+        name: 'iPhone X',
+        styles: {
+          width: '375px',
+          height: '812px',
+        },
+      },
+    },
+  },
+  backgrounds: {
+    values: [
+      {
+        name: 'grey',
+        value: '#808080',
+      }
+    ]
+  }
 }
